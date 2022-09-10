c2c720efc3ddc46083abdfd201547fcf6391406d
godswilleo <godswilleo@gmail.com>
2022-09-10 10:51:27 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+* main - main entry point, prints text
+* Return: 0
+*
+*/
+
+int main()
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
