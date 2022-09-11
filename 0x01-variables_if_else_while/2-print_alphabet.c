c42ecd958cafc16b8e0da71d7803767a5c1650ef
godswilleo <godswilleo@gmail.com>
2022-09-11 14:23:47 -0700

    26 alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+* main - print alphabet from a - z
+* Return: 0
+*
+*/
+
+int main(void)
+{
+	int a_;
+
+	for (a_ = 'a'; a_ < 'z'; a_++)
+	{
+		putchar(a_);
+	}
+	return (0);
+}
+
