82d7dde23ea7a28b452b15e0bb9959007e6025a1
godswilleo <godswilleo@gmail.com>
2022-09-12 02:54:42 -0700

    100-print_comb3

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/**
+* main - prints all possible different combinations of two digits
+* Return: 0
+*
+*/
+
+int main(void)
+{
+	int a;
+	int b;
+
+	for (a = 0; a <= 9; a++)
+	{
+
+		for (b = 0; b <= 9; b++)
+		{
+			if (a >= b)
+			{
+			continue;
+			}
+
+			putchar(a + '0');
+			putchar(b + '0');
+			putchar(',');
+			putchar(' ');
+
+		}
+	}
+
+	putchar('\n');
+	return (0);
+
+}
