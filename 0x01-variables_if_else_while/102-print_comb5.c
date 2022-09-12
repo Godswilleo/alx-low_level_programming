ecbfe4c0bfe2f24b597fc1ac8cb175dc7f94c5dd
godswilleo <godswilleo@gmail.com>
2022-09-12 04:53:02 -0700

    102 print comb5

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+/**
+* main - prints all possible different combinations of two digits
+* Return: 0
+*
+*/
+
+int main(void)
+{
+	int a, b, c, d;
+
+	for (a = 0; a <= 9; a++)
+	{
+		for (b = 0; b <= 9; b++)
+		{
+			for (c = 0; c <= 9; c++)
+			{
+				for (d = 0; d <= 9; d++)
+				{
+					if (((a + b) >= (c + d)) || a > c)
+					{
+					continue;
+					}
+					putchar(a + '0');
+					putchar(b + '0');
+					putchar(' ');
+					putchar(c + '0');
+					putchar(d + '0');
+
+					if (!(a == 9 && b == 8 && c == 9 && d == 9))
+					{
+						putchar(',');
+					}
+				}
+			}
+		}
+	}
+
+	putchar('\n');
+	return (0);
+
+
+}
