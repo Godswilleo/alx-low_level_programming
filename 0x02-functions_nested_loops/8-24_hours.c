c52b0ec31dfdee80b10086b3e29587e07c05195c
godswilleo <godswilleo@gmail.com>
2022-09-13 14:17:13 -0700

    jack bauer

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -10,23 +10,15 @@
 int jack_bauer(void)
 {
 
-	int a, b, c, d;
+	int a, b;
 
-	for (a = 0; a <= 2; a++)
+	for (a = 0; a <= 23; a++)
 	{
-		for (b = 0; b <= 3; b++)
+		for (b = 0; b <= 59; b++)
 		{
 
-			for (c = 0; c <= 5; c++)
-			{
-				for (d = 0; d <= 9; d++)
-				{
+			printf("%d:%d\n", a, b);
 
-				printf("%d%d:%d%d\n", a, b, c, d);
-
-				}
-
-			}
 		}
 
 
