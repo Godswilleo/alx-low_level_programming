360a152123b44b95017642388f43e7a65797f26e
godswilleo <godswilleo@gmail.com>
2022-09-15 13:26:21 -0700

    task 6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+* print_line - print line, line lengh depends of value of n
+* @n: variable to determine the length of line to be drawn
+*
+*/
+
+void print_line(int n)
+{
+	int i = 0;
+
+	if (n <= 0)
+	{
+	putchar('\n');
+	}
+	else
+	{
+		while (i <= n)
+		{
+			putchar('_');
+			i++;
+
+		}
+		putchar('\n');
+	}
+}
