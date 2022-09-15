4c02089e6d4309979c937f0289ffd6d32d1ea910
godswilleo <godswilleo@gmail.com>
2022-09-15 12:48:23 -0700

    task 5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -10,13 +10,22 @@ void more_numbers(void)
 {
 	int i;
 	int j;
+	int k;
 
 	for (i = 0; i < 10; i++)
 	{
 
 		for (j = 0; j <= 14; j++)
 		{
-			printf("%d",j);
+			k = j;
+			if (j > 9)
+			{
+				k = k % 10;
+				putchar('1');
+
+			}
+
+			putchar(k + '0');
 		}
 		putchar('\n');
 
