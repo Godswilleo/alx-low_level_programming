8164c1ff34ecd0677ba8dc8b1877331b9bfe6c56
godswilleo <godswilleo@gmail.com>
2022-09-15 12:54:35 -0700

    task 0

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -3,15 +3,15 @@
 
 /**
 * _isupper - prints the upper case inputer char
-*Return: 1 if input char is uppercase and 0 if it is not
-*
+* Return: 1 if input char is uppercase and 0 if it is not
+* @character: variable to be passed in
 */
 
 int _isupper(char character)
 {
-	if(isupper(character))
+	if (isupper(character))
 		return (1);
 	else
-		return(0);
+		return (0);
 
 }
