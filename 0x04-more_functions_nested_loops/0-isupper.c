9ef8cab5f3268bb02e9fa6b7da52fd8c80849078
godswilleo <godswilleo@gmail.com>
2022-09-15 08:36:48 -0700

    task 0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+* _isupper - prints the upper case inputer char
+*Return: 1 if input char is uppercase and 0 if it is not
+*
+*/
+
+int _isupper(char character)
+{
+
+	if (isupper(character))
+		return (1);
+	else
+		return(0);
+
+}
