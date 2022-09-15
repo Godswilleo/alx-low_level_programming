7150feeea751b6fc4c927fa22ed858bb4ec83054
godswilleo <godswilleo@gmail.com>
2022-09-15 13:01:19 -0700

    task 2

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -2,16 +2,16 @@
 #include <ctype.h>
 
 /**
-* _isupper - prints the upper case inputer char
-*Return: 1 if input char is uppercase and 0 if it is not
-*
+* _isdigit - prints 1 if character is a digit and prints 0 if otherwise
+* Return: 1 if input char is uppercase and 0 if it is not
+* @character: variable to be passed in
 */
 
 int _isdigit(char character)
 {
-	if(isdigit(character))
+	if (isdigit(character))
 		return (1);
 	else
-		return(0);
+		return (0);
 
 }
