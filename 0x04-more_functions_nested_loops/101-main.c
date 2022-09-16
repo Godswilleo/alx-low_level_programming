3b453156cb0a022f1a9155253e7439ae7c2110e5
godswilleo <godswilleo@gmail.com>
2022-09-16 04:27:10 -0700

    9-fizz_buzz task 9

---
+++ 0x04-more_functions_nested_loops/101-main.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* main - check the code
+*
+* Return: Always 0.
+*/
+
+int main(void)
+
+{
+
+	print_number(98);
+
+	putchar('\n');
+
+	print_number(402);
+
+	putchar('\n');
+
+	print_number(1024);
+
+	putchar('\n');
+
+	print_number(0);
+
+	_putchar('\n');
+
+	print_number(-98);
+
+	_putchar('\n');
+
+	return (0);
+
+}
