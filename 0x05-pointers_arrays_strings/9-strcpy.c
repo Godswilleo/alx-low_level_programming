c506b9c9f7a331b2b9463533be43fcf5dc8136d1
godswilleo <godswilleo@gmail.com>
2022-09-20 22:11:06 +0000

    task 9

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,1 +1,26 @@
+#include <stdio.h>
 
+/**
+ * _strcpy - copy the string from src to dest
+ * @dest: the destination string
+ * @src: the source string
+ * Return: dest
+ *
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+
+	int i = -1;
+
+	while (src[i] != '\0')
+	{
+
+		dest[i] = src[i];
+		i++;
+
+	}
+
+	return (dest);
+
+}
