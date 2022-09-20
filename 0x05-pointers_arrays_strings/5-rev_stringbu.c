8d631a734c34a296de5d0c41eedc0bf4508a5bd3
godswilleo <godswilleo@gmail.com>
2022-09-20 21:42:03 +0000

    task added

---
+++ 0x05-pointers_arrays_strings/5-rev_stringbu.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+/**
+ * rev_string - prints strings in reverse
+ * @s: variable containing string to be reversed
+ *
+ */
+
+void rev_string(char *s)
+{
+	int a;
+
+	for (a = (strlen(s)-1); a >= 0; --a)
+	{
+
+		printf("%c", s[a]);
+
+	}
+
+	printf("\n");
+
+}
--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -4,29 +4,22 @@
 
 
 /**
- * puts2 - prints everyother character of a string
- * Return: returns to string length
- * @str: string varaible to be checked
- *
- */
+* puts2 - prints everyother character of a string
+* Return: returns to string length
+* @str: string varaible to be checked
+*
+*/
 
 void puts2(char *str)
 {
 	int a;
 
 	a = 0;
-
 	while (str[a] != '\0')
 	{
 		if ((a % 2) == 0)
-			putchar(str[a]);
-
+		putchar(str[a]);
 		a++;
-
 	}
 	putchar('\n');
-
-
-
-
 }
