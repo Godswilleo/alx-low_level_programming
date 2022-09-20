a4118d3e054f06f9eca1567d5b33458cdf91a862
godswilleo <godswilleo@gmail.com>
2022-09-20 22:26:12 +0000

    task 9

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -13,13 +13,13 @@ char *_strcpy(char *dest, char *src)
 
 	int i = -1;
 
-	while (src[i] != '\0')
+	do
 	{
-
-		dest[i] = src[i];
 		i++;
-
+		dest[i] = src[i];
+	
 	}
+	while (src[i] != '\0');
 
 	return (dest);
 
