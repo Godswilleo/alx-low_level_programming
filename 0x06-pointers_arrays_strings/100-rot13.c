f274a7dc4154f48cbe049c2f5a24b10d42fbfc6e
godswilleo <godswilleo@gmail.com>
2022-09-22 22:02:19 +0000

    100 rot13

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,29 @@
+#include <string.h>
+#include <stdio.h>
+
+/**
+ * rot13 - applies rot13 encryption
+ * Return: nothing
+ * @s: array of alphabets
+ */
+
+char *rot13(char *s)
+{
+	int i;
+
+	char alphabets[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char rot13x[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+
+	for (i = 0; i < 52; i++)
+	{
+		if (s[i] <= 122 && s[i] >= 65)
+		{
+	     	     	rot13x[i] = alphabets[i];
+			s[i] = rot13x[i];
+		}
+			
+		printf("%c", s[i] + '\0');
+	}
+	return (s);
+
+}
