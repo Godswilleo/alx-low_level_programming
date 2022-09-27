270b464dc95859ff40fba4f6ea49522a8d935cbe
godswilleo <godswilleo@gmail.com>
2022-09-27 18:39:40 +0000

    task 7

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,30 @@
+#include <string.h>
+#include <stdio.h>
+/**
+* print_chessboard - prints a chessboard
+* Return: nothing
+* @a: char array
+*
+*/
+
+void print_chessboard(char (*a)[8])
+{
+	int c;
+	int b;
+
+	for (c = 0; c < 8; c++)
+	{
+		for (b = 0; b < 8; b++)
+		{
+			printf("%c ", a[c][b]);
+			if (b == 7)
+			{
+			printf("\n");
+
+			}
+
+		}
+
+	}
+
+}
