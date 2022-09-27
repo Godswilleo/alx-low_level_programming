8d78e69b4472936aef03e19c169a7a3e85b64491
godswilleo <godswilleo@gmail.com>
2022-09-27 13:44:29 +0000

    task 2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,15 @@
+#include <string.h>
+#include <stdio.h>
+/**
+ * _strchr - locates a character in a string
+ * Return: string
+ * @s: string variable
+ * @c: character variable to locate
+ */
+
+char *_strchr(char *s, char c)
+{
+	char *p = strchr(s, c)
+
+	return (p);
+}
