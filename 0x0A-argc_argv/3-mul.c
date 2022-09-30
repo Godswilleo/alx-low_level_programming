3c1fe87c26a5c3f788a85938cfeb279a55642d82
godswilleo <godswilleo@gmail.com>
2022-09-30 18:30:28 +0000

    task 3

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -3,20 +3,27 @@
 
 /**
 * main - prints the result of multiplication of two numbers
-* Return: 0
+* Return: 1 if program doesn't recieve 2 arguments and 0
+* Description: the program assumes the arguments passed in are always integers
 * @argc: count of terminal arguments
 * @argv: pointer to characters of terminal arguments
 */
 
-int main(int argc __attribute__((unused)), char *argv[])
+int main(int argc, char *argv[])
 {
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (0);
+	}
+	else
+	{
+		int result;
 
-	int result;
+		result = atoi(argv[1]) * atoi(argv[2]);
 
-	result = atoi(argv[1]) * atoi(argv[2]);
+		printf("%d\n", result);
 
-	printf("%d\n", result);
-
-	return (0);
-
+		return (0);
+	}
 }
