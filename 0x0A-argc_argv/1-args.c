00093aaf6077d9087d87be776e1e645fa1c5bc77
godswilleo <godswilleo@gmail.com>
2022-09-30 17:59:19 +0000

    task 1:

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+* main - prints the number of arguments passed
+* Return: 0
+* @argc: count of terminal arguments
+* @argv: pointer to characters of terminal arguments
+*/
+
+int main(int argc, char *argv[] __attribute((unused)))
+{
+	printf("%d\n", argc);
+
+	return (0);
+
+}
