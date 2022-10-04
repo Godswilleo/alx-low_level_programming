aedbf1269ea29f12918ee6179ad5c1ab5c3aef00
godswilleo <godswilleo@gmail.com>
2022-10-04 13:25:43 +0000

    0-create-array

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -2,7 +2,9 @@
 
 /**
 * create_array - creates an array and returns a pointer
-*
+* Return: NULL if size is 0
+* c: character passed
+* size: size of array
 */
 
 char *create_array(unsigned int size, char c)
