c068d8249f814c2a720d12157171f1f540201961
godswilleo <godswilleo@gmail.com>
2022-10-04 13:23:24 +0000

    0-create-array

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+* create_array - creates an array and returns a pointer
+*
+*/
+
+char *create_array(unsigned int size, char c)
+{
+
+
+char *i = malloc(sizeof(c), * size)
+	if (i == NULL)
+	{
+		return (NULL);
+	}
+
+
+}
---
+++ 0x0B-malloc_free/README.md
@@ -0,0 +1,1 @@
+# Tasks of Malloc and Free
