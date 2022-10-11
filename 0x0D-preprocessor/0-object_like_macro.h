47b8e8128005ba2becb0401f349a7ba2d9ebec63
godswilleo <godswilleo@gmail.com>
2022-10-11 10:08:47 +0000

    task 0

--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -2,10 +2,11 @@
 #define OBJECT_LIKE_MACRO_H
 
 /**
+* File: 0-object_like_macro.h
 * Description: This file defines a macro SIZE
 */
 
 #define SIZE 1024
 
 
-#endif /* OBJECT_LIKE_MACRO_H */
+#endif
