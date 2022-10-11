1edb3a64aa8ea2ea31a1e863b42fe988b79c7489
godswilleo <godswilleo@gmail.com>
2022-10-11 10:55:38 +0000

    task 3

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,6 +1,14 @@
 #ifndef FUNCTION_LIKE_MACRO_H
 #define FUNCTION_LIKE_MACRO_H
+#include <stdlib.h>
 
+/**
+* File: 3-function_like_macro.h
+* Description: creates a macro for the absolute value of x
+* @x: value whose absolute value is the be evalutated
+*/
+
+#define ABS(x) (abs(x))
 
 
 #endif
