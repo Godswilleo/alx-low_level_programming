e3d2f131060edfee12de3fc7c3f605e01b9227fe
godswilleo <godswilleo@gmail.com>
2022-10-11 11:02:17 +0000

    task 3

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,6 +1,5 @@
 #ifndef FUNCTION_LIKE_MACRO_H
 #define FUNCTION_LIKE_MACRO_H
-#include <stdlib.h>
 
 /**
 * File: 3-function_like_macro.h
@@ -8,7 +7,7 @@
 * @x: value whose absolute value is the be evalutated
 */
 
-#define ABS(x) (abs(x))
+#define ABS(x) ((x) < 0 ? (-x) : (x))
 
 
 #endif
