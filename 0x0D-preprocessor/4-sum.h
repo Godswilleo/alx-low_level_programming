0aa2ea5b91c10f1a39da2b38700efea6d632f017
godswilleo <godswilleo@gmail.com>
2022-10-11 10:42:45 +0000

    task 4

---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,15 @@
+#ifndef SUM_H
+#define SUM_H
+
+/**
+* File: 4-sum.h
+* Description: creates a macro that computes the sum of two variables x and y
+* @x: variable
+* @y: variable
+*/
+
+
+#define SUM(x, y) ((x) + (y))
+
+
+#endif
