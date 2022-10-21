94a0275aaa475e7b74d1b006fd212d7461359bd2
Godswill Enaohwo <godswilleo@yahoo.com>
2022-10-21 16:41:59 +0000

    update

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -12,21 +12,22 @@
 
 list_t *add_node(list_t **head, const char *str)
 {
-	list_t *new_node;
-	size_t n;
+	list_t *new;
 
-	new_node = malloc(sizeof(list_t));
-	if (new_node == NULL)
+	unsigned int n;
+
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
 		return (NULL);
 
-	new_node->str = strdup(str);
+	new->str = strdup(str);
 
 	for (n = 0; str[n]; n++)
-		;
+		len++;
 
-	new_node->len = n;
-	new_node->next = *head;
-	*head = new_node;
+	new->len = n;
+	new->next = *head;
+	*head = new;
 
 	return (*head);
 }
