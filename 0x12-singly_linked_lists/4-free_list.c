a802dd285fbad837846bce2937d39f015ca13529
Godswill Enaohwo <godswilleo@yahoo.com>
2022-10-21 16:48:46 +0000

    free list

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * free_list - frees a list
+ * @head: head of the linked list
+ * Return: no return.
+ */
+
+void free_list(list_t *head)
+{
+	list_t *i;
+
+	while ((i = head) != NULL)
+	{
+		head = head->next;
+
+		free(i->str);
+
+		free(i);
+	}
+}
