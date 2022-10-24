908195ac85bd378a07fd78000d24a4a4bfc389cb
Godswill Enaohwo <godswilleo@yahoo.com>
2022-10-24 20:11:12 +0000

    task 1

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+/**
+ * listint_len - returns then number of elements in a list.
+ * @h: singly linked list.
+ * Return: number of elements in the list.
+ */
+
+size_t listint_len(const listint_t *h)
+{
+	unsigned int n;
+
+	n = 0;
+
+	while (h)
+	{
+
+		h = h->next;
+
+	    n++;
+	}
+
+	return (n);
+}
