a916ac22ed59f10a2cd2bfeab7e22e63cb112859
Godswill Enaohwo <godswilleo@yahoo.com>
2022-10-25 16:33:48 +0000

    print

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -9,11 +9,13 @@ size_t print_listint(const listint_t *h)
 {
 	size_t number_of_nodes = 0;
 
-	while (h)
+	while (h != NULL)
 	{
 		printf("%d\n", h->n);
-		number_of_nodes++;
+		
 		h = h->next;
+		number_of_nodes++
+
 	}
 	return (number_of_nodes);
 }
