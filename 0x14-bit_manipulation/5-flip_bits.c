e2c15cee280873ffc19aaec4f966b4d3669cfdd9
Godswill Enaohwo <godswilleo@yahoo.com>
2022-10-27 15:58:33 +0000

    flip bits

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * get_endianness - Checks the endianness.
+ *
+ * Return: If big-endian - 0 and if little-endian - 1.
+ */
+
+int get_endianness(void)
+{
+	int i = 1;
+	char *endian = (char *)&i;
+
+	if (*endian == 1)
+		return (1);
+
+	return (0);
+}
