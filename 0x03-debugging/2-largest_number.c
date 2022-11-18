8b19e42cfa023d72b95779aeda9c7211a91574a8
Godswill Enaohwo <godswilleo@yahoo.com>
2022-11-18 11:20:57 +0000

    debugg update

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -13,21 +13,17 @@ int largest_number(int a, int b, int c)
 
 {
 
-	int largest;
+	int largest = b;
 
-	if (a > b && a > c)
+	if (a > b)
 	{
 		largest = a;
 	}
-	if (b > a && b > c)
-	{
-		largest = b;
-	}
-
-	if (c > a && c > b)
+	else if (c > b)
 	{
 		largest = c;
 	}
+
 	return (largest);
 }
 
