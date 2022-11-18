413b68a15fe3c5fdfbb2c7eca9068d54c6acf0cb
Godswill Enaohwo <godswilleo@yahoo.com>
2022-11-18 11:41:24 +0000

    2 concat

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -6,5 +6,7 @@ void free_grid(int **grid, int height);
 char *argstostr(int ac, char **av);
 char *create_array(unsigned int size, char c);
 char *_strdup(char *str);
+char *str_concat(char *s1, char *s2);
+
 
 #endif /** MAIN_H */
