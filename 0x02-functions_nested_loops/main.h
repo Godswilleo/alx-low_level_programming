b5ae116595777af37260823f3f62d5e913697b5c
Godswill Enaohwo <godswilleo@yahoo.com>
2022-11-20 20:35:31 +0000

    sign

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -4,7 +4,7 @@ int print_word(char n);
 int _isalpha(int c);
 int print_alphabet(void);
 int print_alphabet_x10(void);
-
+int print_sign(int n);
 int _islower(int c);
 int _putchar(char c);
 int _abs(int num);
